1507bdb095d490284efc2413cb24002f13b83a21
Charles Ibo <charles.ibo.247@my.csun.edu>
2018-09-07 19:23:36 -0700

    Variable scope

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x = 5
+
+func main(){
+	//Testing print statement
+	y := 10
+	y =11
+	fmt.Println(y)
+	charlesTest()
+
+}
+
+func charlesTest(){
+	fmt.Println(x)
+}
