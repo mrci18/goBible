8f7cfbc02b986d49fed3fdd14f5bced3d8d8c2dd
Charles Ibo <charles.ibo.247@my.csun.edu>
2018-09-12 21:14:15 -0700

    html

--- run/html.go
+++ run/html.go
@@ -2,48 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 const (
 	miToKm = 1.60934
-	pToKg = 0.453592
-	divider = "+--------------------------------------------------------+"
 )
 
 func main(){
-	fmt.Println("Choose an option ")
-	fmt.Println("1: Miles to kilometers: ")
-	fmt.Println("2: Fahrenheit to Celcius")
-	fmt.Println("3: Pounds to kilograms")
-
-	var (
-		option int
-		number float64
-	)
-
-	fmt.Scanln(&option)
-
-	// fmt.Println("Enter a number to convert: ")
-	// fmt.Scanf("%f", &number)
-
-	fmt.Println(divider)
-	switch option {
-	case 1: 
-		fmt.Scanln(&number)
-		fmt.Printf("| Miles: %15.2f |\n", number)
-		fmt.Println(divider)
-		fmt.Printf("| Kilometers: %10.2f |\n", number*miToKm)
-	case 2: 
-		fmt.Scanln(&number)
-		fmt.Printf("| Fahrenheit: %.2f |\n", number)
-		fmt.Println(divider)
-		fmt.Printf("Celsius: %13.2f |\n", (number-32)*5/9)
-	case 3: 
-		fmt.Scanln(&number)
-		fmt.Printf("| Pounds: %.2f | \n", number)
-		fmt.Println(divider)
-		fmt.Printf("| Kilos: %.2f | \n", number*pToKg)
+	number, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println(divider)
-}
+	//get the number of miles from the user
+	// var number float64
+	// fmt.Scanln(&number)
 
+	fmt.Println("<!DOCTYPE html>")
+	fmt.Println("<html>")
+	fmt.Println("<head></head>")
+	fmt.Println("<body>")
+
+
+	fmt.Printf("Miles: %.2f<br>\n", number)
+	fmt.Printf("Kilometers: %.2f <br>\n", number*miToKm)
+
+	fmt.Println("</body>")
+
+	fmt.Println("</html>")
+}
