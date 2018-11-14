eff60623c0aecf3a76950434c2196087891f2fda
Charles Ibo <charles.ibo.247@my.csun.edu>
2018-11-14 00:03:40 -0800

    Pass data into template

---
+++ data/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+
+}
+
+func main() {
+	f1, err := os.Create("default.html")
+	if err != nil {
+		log.Fatal("f1 errored", err)
+	}
+
+	defer f1.Close()
+
+	f2, err := os.Create("default.html")
+	if err != nil {
+		log.Fatal("f1 errored", err)
+	}
+
+	defer f2.Close()
+
+	xi := []int{7, 8, 9, 125, 245,}
+	err = tpl.ExecuteTemplate(f1, "default.gohtml", "Charles")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tpl.ExecuteTemplate(f1, "about.gohtml", xi)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
